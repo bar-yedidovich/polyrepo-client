402024026e37a1e5b78d6582839d19bf2fd3803a
Bar Yedidovich <bar.yedidovich@ls-techs.com>
2026-05-15 12:38:04 +0300

    added prettier

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/** @type {import('prettier').Config} */
+export default {
+	useTabs: true,
+	tabWidth: 4,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	printWidth: 100,
+	bracketSpacing: true,
+	arrowParens: 'always',
+	jsxSingleQuote: false,
+	bracketSameLine: false,
+	quoteProps: 'as-needed',
+	endOfLine: 'lf',
+	singleAttributePerLine: true,
+};
